6181abe7982e4f9068cc0316373c1219805e5297
tahasheikh512-stack <tahasheikh512@gmail.com>
2026-05-12 13:54:32 +0500

    Update rm_hierarchy_ntb_report.sql

--- SPM-ProjectAAML/rm_hierarchy_ntb_report.sql
+++ SPM-ProjectAAML/rm_hierarchy_ntb_report.sql
@@ -10,22 +10,21 @@ KEY FEATURES:
 - Joins with NTB View (view_RM_NTB_DATA)
 */
 
-WITH rm_hierarchy AS (
-    SELECT ss.si04_id login_id,
-           ss.si04_report_to parent_id,
-           (SELECT a.si04_first_name || ' ' || a.si04_middle_name || ' ' || a.si04_last_name
-            FROM si04_employee a
-            WHERE a.si04_id = ss.si04_report_to) parent_name,
-           ss.si04_first_name descrip,
-           LEVEL AS lvl,
-           ss.crm_status_id
+with rm_hierarchy as
+(SELECT ss.si04_id login_id,
+         ss.si04_report_to parent_id,
+         (select a.si04_first_name || ' ' || a.si04_middle_name || ' ' ||
+                 a.si04_last_name
+            from si04_employee a
+           where a.si04_id = ss.si04_report_to) parent_name,
+         ss.si04_first_name descrip,
+         LEVEL AS lvl,ss.crm_status_id
     FROM si04_employee ss
-    WHERE ss.crm_status_id NOT IN (9)
-    START WITH ss.si04_id = &P_RM_ID
-    CONNECT BY PRIOR ss.si04_id = ss.si04_report_to
-)
-SELECT rh.*, 
-       vw.NTB 
-FROM view_RM_NTB_DATA vw
-JOIN rm_hierarchy rh ON rh.login_id = vw.si04_employee_id
-ORDER BY rh.lvl;
+    where ss.crm_status_id not in (9)
+   START WITH ss.si04_id = &P_RM_ID
+  CONNECT BY PRIOR ss.si04_id = ss.si04_report_to)
+
+select rh.*,vw.account_opening_date,vw.NTB from view_RM_NTB_DATA vw
+join rm_hierarchy rh
+on rh.login_id = vw.si04_employee_id
+order by rh.lvl;
